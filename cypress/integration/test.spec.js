3555500330087ee43dd63738bb1cfed17c5beeb3
sidraKhursheed <sidrakhursheed90@gmail.com>
2021-09-28 18:51:06 +0200

    Mocha Awesome report and GIT Hub integrated for CI
    Mocha Awesome report and GIT Hub integrated for CI

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -32,3 +32,31 @@ describe('Login Functionality',()=>{
     })
 
 })
+
+describe('ApplyForLeave',()=>{
+    beforeEach(()=>{
+
+        cy.visit("/")
+     
+    })
+    
+    it.only('Succesful Levae Application', () =>{
+       
+        cy.fixture('example').then(function(data){
+        
+        this.data=data
+        cy.login(this.data.username,this.data.password)
+        cy.get('h1').contains("Dashboard")
+        cy.get('#menu_leave_viewLeaveModule > b').click();
+        cy.get('#menu_leave_viewLeaveList').click();
+        cy.url().should('eq',"https://opensource-demo.orangehrmlive.com/index.php/leave/viewLeaveList")       
+        })
+        
+    })
+    
+        
+    afterEach(()=>{
+        cy.logout()
+    })
+
+})
